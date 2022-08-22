4a3cfbc5cb6a02cfb9f8e03884f1e8af4de9adab
Unkkun <111712795+Unkkun@users.noreply.github.com>
2022-08-22 08:27:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+birthday = os.environ['BIRTHDAY1']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
